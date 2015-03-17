0a2d8221764614cb1fd503e034177bc0a735fcdb
田口宗孝 <m.taguchi@leihauoli.com>
2015-03-17 19:29:44 +0900

    update files -> add files social

--- js/page.post.js
+++ js/page.post.js
@@ -6,49 +6,50 @@ var TENSHIN = TENSHIN || {};
 
 TENSHIN.POST = {};
 
-TENSHIN.POST.FIXSIDE = {
+TENSHIN.POST.FIXHEADER = {
 
-    SCROLL_IGNITION_POINT : 1200,
+	SCROLL_IGNITION_POINT : 800,
 
-    init : function(){
-        this.setParameters();
-        this.prepare();
-        this.bindEvent();
-    },
-    setParameters : function(){
-        this.$window = $(window);
-        this.$fixSide = $('.jsc-fix-side');
-        this.showFlag = false;
-        this.$body = $('body');
-    },
-    prepare : function(){
-        var _self = this;
-        this.$window.on('load',function(){
-            _self.$fixSide.css('top','-541px')
-        })
-    },
-    bindEvent : function(){
-        this.$window.on('scroll', $.proxy(this.showHeader, this));
-    },
-    showHeader : function(){
-        if(this.$window.scrollTop() > this.SCROLL_IGNITION_POINT){
-            if(this.showFlag == false){
-                this.showFlag = true;
-                this.$fixSide.stop().animate({'top':'0'},300);
-                if(0 < $(".top-ua").size()){
-                    this.$fixSide.stop().animate({'top':'300'},300);
-                }
-            }
-        }else{
-            if(this.showFlag == true){
-                this.showFlag = false;
-                this.$fixSide.stop().animate({'top':'-541px'},300);
-            }
-        }
-    }
+	init : function(){
+		this.setParameters();
+		this.prepare();
+		this.bindEvent();
+	},
+	setParameters : function(){
+		this.$window = $(window);
+		this.$fixHeader = $('#jsi-fix-header');
+		this.showFlag = false;
+		this.$body = $('body');
+	},
+	prepare : function(){
+		var _self = this;
+		this.$window.on('load',function(){
+			_self.$fixHeader.css('top','-60px')
+		})
+	},
+	bindEvent : function(){
+		this.$window.on('scroll', $.proxy(this.showHeader, this));
+	},
+	showHeader : function(){
+		if(this.$window.scrollTop() > this.SCROLL_IGNITION_POINT){
+			if(this.showFlag == false){
+				this.showFlag = true;
+				this.$fixHeader.stop().animate({'top':'0'},300);
+				if(0 < $(".top-ua").size()){
+					this.$fixHeader.stop().animate({'top':'300'},300);
+				}
+			}
+		}else{
+			if(this.showFlag == true){
+				this.showFlag = false;
+				this.$fixHeader.stop().animate({'top':'-60px'},300);
+			}
+		}
+	}
 };
 
 
-$(window).on('load',function(){
-    TENSHIN.POST.FIXSIDE.init();
+
+$(function(){
+	TENSHIN.POST.FIXHEADER.init();
 });
